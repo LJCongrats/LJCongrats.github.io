db17baed3697fb90a93704e29b05330c70f140f1
LJCongrats <105575131+LJCongrats@users.noreply.github.com>
2024-05-03 21:05:33 +0800

    Update download.js

--- download.js
+++ download.js
@@ -13,7 +13,7 @@ function downloadData() {
     var loadingSpinner = document.getElementById('loadingSpinner');
     loadingSpinner.style.display = 'block';
 
-    fetch("https://5eed-140-116-156-231.ngrok-free.app/download", {
+    fetch("https://2772-140-116-156-231.ngrok-free.app/download", {
         method: 'POST',
         body: requestData
     })
