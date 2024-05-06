c3c93a6709cf2077ca4f360f4dc309c4024e2af0
LJCongrats <105575131+LJCongrats@users.noreply.github.com>
2024-05-06 20:22:54 +0800

    Update download.js

--- download.js
+++ download.js
@@ -13,7 +13,7 @@ function downloadData() {
     var loadingSpinner = document.getElementById('loadingSpinner');
     loadingSpinner.style.display = 'block';
 
-    fetch("https://c712-140-116-156-231.ngrok-free.app/download", {
+    fetch("https://455e-140-116-156-231.ngrok-free.app/download", {
         method: 'POST',
         body: requestData
     })
