3047eb0854b6dd426df9ecb2238a7f9c9faab435
LJCongrats <105575131+LJCongrats@users.noreply.github.com>
2024-09-03 13:05:53 +0800

    Update DrugAllergy.js

--- DrugAllergy.js
+++ DrugAllergy.js
@@ -4,7 +4,7 @@ function uploadFiles() {
 
             var userData = document.getElementById("AllergyIntolerance").value;
 
-            const api_url = "https://e1dc-140-116-156-231.ngrok-free.app//input_DrugAllergy";
+            const api_url = "https://0db7-140-116-156-231.ngrok-free.app//input_DrugAllergy";
 
             const formData = new FormData();
             formData.append('userData', userData);
