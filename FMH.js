2a0b63926c961f17fae26135531cff639763c9a1
LJCongrats <105575131+LJCongrats@users.noreply.github.com>
2025-03-31 10:54:57 +0800

    Update FMH.js

--- FMH.js
+++ FMH.js
@@ -4,7 +4,7 @@ function uploadFiles() {
 
             var userData = document.getElementById("familyhistory").value;
 
-            const api_url = "https://0db7-140-116-156-231.ngrok-free.app/input_FMH";
+            const api_url = "https://02f9-140-116-156-231.ngrok-free.app/input_FMH";
 
             const formData = new FormData();
             formData.append('userData', userData);
@@ -49,7 +49,7 @@ function deleteEntry(entryId) {
             // 清空上次回應的訊息
             document.getElementById("responseMessage").innerHTML = '';
 
-            const api_url = "https://e1dc-140-116-156-231.ngrok-free.app/delete";
+            const api_url = "https://02f9-140-116-156-231.ngrok-free.app/delete";
 
             const formData = new FormData();
             formData.append('Resource', "FamilyMemberHistory");
