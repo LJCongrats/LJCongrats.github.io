8a934f0232a5e4722610d3794e96622eb2bc740d
LJCongrats <105575131+LJCongrats@users.noreply.github.com>
2025-03-31 11:02:45 +0800

    Update smoke.js

--- smoke.js
+++ smoke.js
@@ -4,7 +4,7 @@ function uploadFiles() {
 
             var userData = document.getElementById("smoking").value;
 
-            const api_url = "https://0db7-140-116-156-231.ngrok-free.app/input_Smoking";
+            const api_url = "https://02f9-140-116-156-231.ngrok-free.app/input_Smoking";
 
             const formData = new FormData();
             formData.append('userData', userData);
@@ -49,7 +49,7 @@ function deleteEntry(entryId) {
             // 清空上次回應的訊息
             document.getElementById("responseMessage").innerHTML = '';
 
-            const api_url = "https://0db7-140-116-156-231.ngrok-free.app/delete";
+            const api_url = "https://02f9-140-116-156-231.ngrok-free.app/delete";
 
             const formData = new FormData();
             formData.append('Resource', "Condition");
