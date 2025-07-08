16b47988adf88f726045f8f5d604690fb9f7118c
LJCongrats <105575131+LJCongrats@users.noreply.github.com>
2025-07-08 13:49:18 +0800

    Update FMH.js

--- FMH.js
+++ FMH.js
@@ -49,7 +49,7 @@ function deleteEntry(entryId) {
             // 清空上次回應的訊息
             document.getElementById("responseMessage").innerHTML = '';
 
-            const api_url = "https://02f9-140-116-156-231.ngrok-free.app/delete";
+            const api_url = `${API_BASE_URL}/delete`;
 
             const formData = new FormData();
             formData.append('Resource', "FamilyMemberHistory");
